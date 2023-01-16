1ca126e80fa699a578fa2d338bfa60085f2fbf74
PAVAN903 <navap903@gmail.com>
2023-01-16 16:41:39 +0530

    validating last name

--- src/com/bridgelabday23/Main.java
+++ src/com/bridgelabday23/Main.java
@@ -5,14 +5,22 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	Firstname user=new Firstname();
+	UserRegistration user=new UserRegistration();
 	Scanner sc=new Scanner(System.in);
 	System.out.println("enter first name");
 	String fname=sc.next();
-	if(user.firstname(fname)==true) {
-		System.out.println("valid first name");
+	if(user.valid(fname)==true) {
+		System.out.println("first name is valid");
 	}else {
-		System.out.println("invalid first name");
+		System.out.println("first name is not valid");
+	}
+	System.out.println("enter lastname");
+	String lname=sc.next();
+	if(user.valid(lname)==true) {
+		System.out.println("last name is valid");
+	}else {
+		System.out.println("last name is not valid");
+	
 	}
 
 	}
--- src/com/bridgelabday23/UserRegistration.java
+++ src/com/bridgelabday23/UserRegistration.java
@@ -2,12 +2,14 @@ package com.bridgelabday23;
 
 import java.util.regex.Pattern;
 
-public class Firstname {
-	boolean firstname(String Firstname) {
+public class UserRegistration {
+	boolean valid(String name)
+	
+	
+    {
 		Pattern pattern=Pattern.compile("^[A-Z]{1}[a-zA-Z]{3,}");
-		boolean matches=pattern.matcher(Firstname).matches();
+		boolean matches=pattern.matcher(name).matches();
 		return matches;
 		
 	}
-
-}
+	}
